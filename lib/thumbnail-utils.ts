9e58472311645fa9c3fcce8a87316950497914f1
Pankaj Chouksey <pankajchouksey521@gmail.com>
2025-12-01 23:01:38 +0530

    Fix TypeScript error: Update thumbnail-utils to support all ContentType values

--- lib/thumbnail-utils.ts
+++ lib/thumbnail-utils.ts
@@ -1,12 +1,16 @@
+import { ContentType } from "./types"
+
 /**
  * Get the default thumbnail path for a content type
  */
-export function getDefaultThumbnail(type: "note" | "video" | "pyq" | "important"): string {
-  const thumbnails = {
+export function getDefaultThumbnail(type: ContentType): string {
+  const thumbnails: Record<ContentType, string> = {
     note: "/thumbnails/note-default.svg",
     video: "/thumbnails/video-default.svg",
     pyq: "/thumbnails/pyq-default.svg",
     important: "/thumbnails/important-default.svg",
+    syllabus: "/thumbnails/note-default.svg", // Using note thumbnail as fallback
+    timetable: "/thumbnails/note-default.svg", // Using note thumbnail as fallback
   }
   
   return thumbnails[type]
@@ -17,7 +21,7 @@ export function getDefaultThumbnail(type: "note" | "video" | "pyq" | "important"
  */
 export function getThumbnailUrl(
   thumbnail: string | undefined | null,
-  type: "note" | "video" | "pyq" | "important"
+  type: ContentType
 ): string {
   return thumbnail || getDefaultThumbnail(type)
 }
